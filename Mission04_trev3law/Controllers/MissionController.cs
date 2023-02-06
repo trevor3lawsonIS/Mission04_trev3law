1557fade4521548da6f5dfb08629ed9e896c4d63
Trevor_Lawson <trevor3lawson@gmail.com>
2023-02-06 13:55:25 -0700

    FIrst MVC

---
+++ Mission04_trev3law/Controllers/MissionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_trev3law.Controllers
+{
+    public class MissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+    }
+}
--- Mission04_trev3law/Startup.cs
+++ Mission04_trev3law/Startup.cs
@@ -16,12 +16,12 @@ namespace Mission04_trev3law
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             if (env.IsDevelopment())
@@ -33,10 +33,10 @@ namespace Mission04_trev3law
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Mission}/{action=index}/{id?}"
+                );
             });
         }
     }
